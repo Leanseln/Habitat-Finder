607ae84ba7544bddb7a2709f23e4a453a6df3712
Leanseln <oleanselnico@gmail.com>
2024-02-06 12:39:22 +0800

    Pages and Routes

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
