ccc4c3236c9ff8c8a16cb7f46e6d674582ab11fa
Adriyan06 <159153919+Adriyan06@users.noreply.github.com>
2024-02-11 15:02:25 +0800

    Adrian Commit

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,59 @@
-import Header from "../components/Header"
+
 
 const LandingPage = () => {
     return (
         
         <>
             
-            <div>
-                <h1 className="text-2xl">Looking for House?</h1>
-            </div>
+            <div className="flex justify-center">
+        <div className="bg-gray-200 p-6 rounded-lg text-center mt-8 transform -translate-y-10" style={{ width: '75%', transform: 'scaleY(1.1)' }}>
+          <p className="text-5xl text-gray-800 font-semibold">
+            Looking for a House?
+          </p>
+          <p className="text-lg text-gray-800 font-semibold text-justify mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+            urna et nisi facilisis finibus. In hac habitasse platea dictumst.
+            Pellentesque habitant morbi tristique senectus et netus et
+            malesuada fames ac turpis egestas. Integer fringilla odio ut neque
+            ullamcorper, sit amet fermentum ligula vulputate. Ut in leo nec
+            turpis posuere malesuada et eget purus. Nam ut ante sit amet justo
+            fermentum venenatis ut nec nulla. Nullam vestibulum enim in nisi
+            sodales, non pharetra justo ultrices. Vivamus ultrices urna id
+            purus ullamcorper, et bibendum metus posuere.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="bg-gray-200 p-6 rounded-lg text-left mt-14 transform -translate-y-10" style={{ width: '75%', transform: 'scaleY(1.1)' }}>
+          <p className="text-3xl text-gray-800 font-semibold">
+            Real Community
+          </p>
+          <p className="text-3xl text-gray-800 font-semibold">
+            Real Connections
+          </p>
+          <p className="text-lg text-gray-800 font-semibold text-justify mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+            urna et nisi facilisis finibus. In hac habitasse platea dictumst.
+            Pellentesque habitant morbi tristique senectus et netus et
+            malesuada fames ac turpis egestas. Integer fringilla odio ut neque
+            ullamcorper, sit amet fermentum ligula vulputate. Ut in leo nec
+            turpis posuere malesuada et eget purus. Nam ut ante sit amet justo
+            fermentum venenatis ut nec nulla. Nullam vestibulum enim in nisi
+            sodales, non pharetra justo ultrices. Vivamus ultrices urna id
+            purus ullamcorper, et bibendum metus posuere.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <p className="text-lg text-gray-800 font-semibold text-justify mt-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut urna et nisi facilisis finibus.
+          In hac habitasse <br />
+          platea dictumst. Pellentesque habitant morbi tristique senectus
+          et netus et malesuada fames ac turpis egestas.
+        </p>
+      </div>
             
         </>
     )
