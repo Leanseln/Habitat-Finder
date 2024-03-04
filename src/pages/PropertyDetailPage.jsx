eaa18b6941cba7228d28a218d9d757c246fa1d96
Leanseln <oleanselnico@gmail.com>
2024-03-04 19:51:11 +0800

    update

--- src/pages/PropertyDetailPage.jsx
+++ src/pages/PropertyDetailPage.jsx
@@ -35,7 +35,7 @@ const PropertyDetailPage = () => {
     return (
         <>
             <HomeHeader />
-            <div className='mx-auto bg-[#FEECDB]'>
+            <div className='mx-auto bg-[#FEECDB] py-5'>
             <div className="container mx-auto h-screen flex justify-center items-center">
             <div className='flex flex-col '>
                 <div className='mx-auto bg-[#ffd5b2] px-5'>
